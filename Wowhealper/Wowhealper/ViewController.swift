789b0dbc09679436f697bac1bdcce07a32a13b2d
S <sunchengxin@fenrir-tec.com>
2022-07-08 09:37:15 +0800

    新規ベース作成

---
+++ Wowhealper/Wowhealper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wowhealper
+//
+//  Created by 金斗石 on 2022/7/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
